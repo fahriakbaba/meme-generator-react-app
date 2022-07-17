d32a6af533944537a0371ac12ffe4ddaa08ca88b
Fahri Akbaba <fahriakbaba@gmail.com>
2022-07-17 20:38:52 +0300

    Meme-Generator | Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className='header'>
+        <img className='header-image' src="https://seeklogo.com/images/T/troll-face-logo-A3A07A678C-seeklogo.com.png" alt="" />
+        <h2 className='header-ttile'>Meme Genarator</h2>
+        <h4>React Course Project</h4>
+    </header>
+  )
+}
+
+export default Header
