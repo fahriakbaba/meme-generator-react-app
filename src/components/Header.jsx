539a45a0bffaf4db48e59ad29572a41735d714ec
Fahri Akbaba <fahriakbaba@gmail.com>
2022-07-19 17:12:37 +0300

    Meme-Generator | added media query

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className='header'>
         <img className='header-image' src="https://seeklogo.com/images/T/troll-face-logo-A3A07A678C-seeklogo.com.png" alt="" />
-        <h2 className='header-ttile'>Meme Genarator</h2>
+        <h2>Meme Genarator</h2>
         <h4>React Course Project</h4>
     </header>
   )
